5aff940263232e891576b7dc8158e59c1d9069a1
Damien Saint-sever <damiensaintsever@hotmail.fr>
2022-03-03 12:03:51 +0100

    Ajout Regex

--- front/js/cart.js
+++ front/js/cart.js
@@ -90,19 +90,20 @@ for (let item in itemJson) {
             input.setAttribute("class", "itemQuantity")
 
             input.addEventListener("change", function () {
-                quantity.innerHTML = this.value
-
+                quantity.innerHTML = this.value 
+                
                 // Changement de quantité dans le Local Sotrage
                 let updateCart = JSON.parse(localStorage.getItem("cart"))
                 updateCart[item].quantity = this.value
                 localStorage.setItem("cart", JSON.stringify(updateCart))
             })
 
-            // Suppressio d un Article
+            // Suppression d un Article
 
             supp.addEventListener("click", function () {
                 console.log(itemJson.indexOf(itemJson[item]));
-
+                alert(" Le produit est supprimé")
+                location.reload()
                 itemJson.splice(itemJson.indexOf(itemJson[item]), 1)
                 localStorage.setItem("cart", JSON.stringify(itemJson))
 
@@ -120,50 +121,80 @@ for (let item in itemJson) {
 
             //Total Prix
             let totalPrice = []
+            console.log(totalPrice);
             let totalPriceByProduct = []
             totalPriceByProduct = [product.price * itemJson[item].quantity]
-            for (let i = 0; i < itemJson.length; i++) {
-                totalPrice.push(product.price * itemJson[i].quantity)
+            console.log(totalPriceByProduct);
+            
+            totalPrice.push(Number(totalPriceByProduct))
 
-            }
+            
             let priceMax = totalPrice.reduce((a, b) => a + b, 0)
+            console.log(priceMax);
             let totalP = document.getElementById("totalPrice")
             totalP.innerHTML = priceMax
-
-
-
-
 
         })
 
 }
+
+
 // Passer la commande 
 let order = document.getElementById("order")
 order.addEventListener("click", function (e) {
     e.preventDefault()
     console.log("order");
     let contact = {
-        firstname: document.getElementById("firstName").value,
-        name: document.getElementById("lastName").value,
-        adress: document.getElementById("address").value,
+        firstName: document.getElementById("firstName").value,
+        lastName: document.getElementById("lastName").value,
+        address: document.getElementById("address").value,
         city: document.getElementById("city").value,
-        email: document.getElementById("email").value
+        email: document.getElementById("email").value,
+       
     }
-    console.log(contact);
+    // Traitement des erreurs dans le formulaire via Regx
+    if (/^[\w\W]{1,}$/.test(firstName.value)){
+    }
+    else{
+        window.alert("Erreur Prénom");
+    }
 
-    let productTab = []
+    if (/^[\w\W]{1,}$/.test(lastName.value)){
+    }
+    else{
+        window.alert("Erreur Nom")
+    }
+
+    if (/^[\w\W ]{1,}$/.test(address.value)){
+
+    }
+    else{
+        window.alert("Erreur adresse")
+    }
+
+    if (/^[\w\W ]{1,}$/.test(city.value)){
+
+    }
+    else{
+        window.alert("Erreur Ville")
+    }
+    if (/^[\w]+[@]{1}[\w]+[.]{1}[\w]{1,}$/.test(email.value)){
+
+    }
+    else{
+        window.alert("Erreur email")
+    }
+
+    // Envoi de la demande de commande
+    let products = []
     for (let i = 0; i < itemJson.length; i++) {
-        productTab.push(itemJson[i].id)
+        products.push(itemJson[i].id)
     }
-    console.log(productTab);
-
-    let command = JSON.stringify(contact) + productTab
-    console.log({ command });
-
+    console.log(products);
     
     fetch("http://localhost:3000/api/products/order", {
           method: "POST",
-          body : JSON.stringify({ contact, productTab }),
+          body : JSON.stringify({ contact, products }),
           headers: {
               "Accept": "application/json",
               "Content-Type": "application/json"
@@ -178,7 +209,8 @@ order.addEventListener("click", function (e) {
             }
 
         })
-        .then(function () {
+        .then(function (order) {
+            console.log(order);
             document.getElementById("order")
         })
 
